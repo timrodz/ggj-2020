947d05703494f336754d3f39b1409b9108d8f590
Juan Alejandro Morais <timrodz@icloud.com>
2020-02-02 14:59:30 +1300

    Add audio

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 [System.Serializable]
 public class AudioFile {
-	public AudioClip Clip;
-	public string id;
+    public AudioClip Clip;
+    public string id;
 }
 
-public class AudioManager : Singleton<AudioManager>
-{
+public class AudioManager : Singleton<AudioManager> {
     // (Optional) Prevent non-singleton constructor use.
-    protected AudioManager() { }
+    protected AudioManager () { }
 
-	[SerializeField]
+    [SerializeField]
     private AudioSource FX;
 
-	public List<AudioFile> audioFiles = new List<AudioFile>();
+    public List<AudioFile> audioFiles = new List<AudioFile> ();
 
-	public void PlayFX(string id) {
-		var af = audioFiles.Find((file) => file.id == id);
-		FX.PlayOneShot(af.Clip);
-	}
+    public void PlayFX (string id) {
+        AudioFile af = audioFiles.Find ((file) => file.id == id);
+        if (af.Clip != null) {
+            FX.PlayOneShot (af.Clip);
+        }
+    }
 }
