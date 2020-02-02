327618af25e9b35318e0039c8a2c50c822b388e3
OlivierYC <olivier.y@chrun.com>
2020-02-02 13:24:22 +1300

    Mourner prefab Longer start screen

---
+++ Assets/Scripts/MournerMoods.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MournerMoods : MonoBehaviour
+{
+
+
+        public GameObject happyFace;
+        public GameObject sadFace;
+
+
+
+
+
+
+}
